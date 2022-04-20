d5380cc188a65fd508c1328277b8f7e761315148
Chuck-rgb <79595583+Chuck-rgb@users.noreply.github.com>
2022-04-20 11:15:15 -0400

    Added Dynmap to web-server
    yeah. this is a test.

---
+++ dynmap/standalone/dynmap_access.php
@@ -0,0 +1,5 @@
+<?php
+$tilespath = 'C:\\Users\\Chargles\\Documents\\GitHub\\Chuck-rgb.github.io\\dynmap\\tiles/';
+$markerspath = 'C:\\Users\\Chargles\\Documents\\GitHub\\Chuck-rgb.github.io\\dynmap\\tiles/';
+$webpath = 'C:\\Users\\Chargles\\Documents\\GitHub\\Chuck-rgb.github.io\\dynmap/';
+?>
