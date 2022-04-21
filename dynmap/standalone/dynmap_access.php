64f93ae4aa9a90e0389e2488e5b12a2672365114
Vykonn <79595583+Vykonn@users.noreply.github.com>
2022-04-21 11:19:17 -0400

    Delete Dynmap

--- dynmap/standalone/dynmap_access.php
+++
@@ -1,5 +0,0 @@
-<?php
-$tilespath = 'C:\\Users\\Chargles\\Documents\\GitHub\\Chuck-rgb.github.io\\dynmap\\tiles/';
-$markerspath = 'C:\\Users\\Chargles\\Documents\\GitHub\\Chuck-rgb.github.io\\dynmap\\tiles/';
-$webpath = 'C:\\Users\\Chargles\\Documents\\GitHub\\Chuck-rgb.github.io\\dynmap/';
-?>
